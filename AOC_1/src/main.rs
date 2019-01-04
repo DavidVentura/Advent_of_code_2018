7a6e3b5f60a2701875d6f9c4fceda5b6153f8d67
david <davidventura27@gmail.com>
2019-01-04 23:50:51 +0100

    Move obtaining the challenge data into a function

--- AOC_1/src/main.rs
+++ AOC_1/src/main.rs
@@ -1,17 +1,30 @@
 use reqwest;
 use reqwest::header::COOKIE;
 use std::env;
+use url::Url;
 
-fn main() {
-    let challenge_url = "https://adventofcode.com/2018/day/1/input";
+fn get_challenge_data(number: u8) -> String {
+    let url = format!(
+        "https://adventofcode.com/2018/day/{}/input",
+        number.to_string()
+    );
+    let challenge_url = Url::parse(&url).unwrap();
     let session = env::var("AOC_SESSION").unwrap();
     let client = reqwest::Client::new();
     let body = client
         .get(challenge_url)
         .header(COOKIE, String::from("session=".to_owned() + &session))
-        .send().unwrap().text().unwrap();
+        .send()
+        .unwrap()
+        .text()
+        .unwrap();
     let body = body.trim();
     assert!(body.contains("Puzzle inputs differ by user") == false);
+    body.to_string()
+}
+
+fn main() {
+    let body = get_challenge_data(1);
     let list: Vec<i32> = body.split('\n').map(|x| x.parse().unwrap()).collect();
     let result: i32 = list.iter().sum();
     println!("{:?}", list);
