0f2a98b5f37275db479075f7bf0179cd4d264693
david <davidventura27@gmail.com>
2019-01-05 15:33:39 +0100

    add aux functions for aoc2

--- src/aoc_2.rs
+++ src/aoc_2.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-fn part_1(data: &Vec<String>) -> u32 {
+pub fn part_1(data: &Vec<String>) -> u32 {
     let mut twos: u32 = 0;
     let mut threes: u32 = 0;
 
@@ -36,12 +36,64 @@ fn part_1(data: &Vec<String>) -> u32 {
     twos * threes
 }
 
-pub fn part_2(data: &Vec<String>) -> i32 {
-    5
+pub fn distance(word1: &str, word2: &str) -> u32 {
+    assert_eq!(word1.len(), word2.len());
+    let mut wchars1 = word1.chars();
+    let mut wchars2 = word2.chars();
+    let mut distance = 0;
+    for i in 0..word1.len() {
+        let c1 = wchars1.next();
+        let c2 = wchars2.next();
+        if c1 != c2 {
+            distance += 1;
+        }
+    }
+    distance
+}
+pub fn common_chars(word1: &str, word2: &str) -> String {
+    assert_eq!(word1.len(), word2.len());
+    let mut wchars1 = word1.chars();
+    let mut wchars2 = word2.chars();
+    let mut word: String = String::new();
+    for i in 0..word1.len() {
+        let c1 = wchars1.next().unwrap();
+        let c2 = wchars2.next().unwrap();
+        if c1 == c2 {
+            word.push(c1);
+        }
+    }
+    word
+}
+
+pub fn closest_word(data: &Vec<String>, word: &str) -> String {
+    let mut _distance = std::u32::MAX;
+    let mut ret: &str = word;
+    for w in data {
+        if w == word {
+            continue;
+        }
+        let d = distance(w, word);
+        if d < _distance {
+            _distance = d;
+            ret = w;
+        }
+    }
+    ret.to_string()
+}
+
+pub fn part_2(data: &Vec<String>) -> u32 {
+    let closest_words = data.iter().map(|w| (w, closest_word(data, w)));
+    println!("{:?}", closest_words);
+    let distances = closest_words.map(|(w, c)| (w, c.clone(), distance(w, &c)));
+    println!("{:?}", distances);
+    0
 }
 
 pub fn main() {
     let body = super::get_challenge_data(2);
     let data: Vec<String> = body.split('\n').map(|x| x.to_string()).collect();
-    part_1(&data);
+    //part_1(&data);
+    //println!("{}", );
+    //println!("{}",
+    part_2(&data);
 }
