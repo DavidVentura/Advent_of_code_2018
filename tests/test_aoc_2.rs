bdaf7d98bd476b804925eabcdff085596f505c15
david <davidventura27@gmail.com>
2019-01-05 15:33:50 +0100

    add test for aux functions in aoc2

--- tests/test_aoc_2.rs
+++ tests/test_aoc_2.rs
@@ -15,4 +15,24 @@ mod tests {
         .to_vec();
         assert_eq!(aoc_2::part_1(&data), 12);
     }
+    #[test]
+    #[should_panic]
+    fn test_distance_fails_length() {
+        aoc_2::distance("abc", "abcd");
+    }
+    #[test]
+    fn test_distance() {
+        assert_eq!(aoc_2::distance("abc", "abc"), 0);
+        assert_eq!(aoc_2::distance("abc", "abb"), 1);
+        assert_eq!(aoc_2::distance("abc", "acb"), 2);
+        assert_eq!(aoc_2::distance("abc", "cab"), 3);
+    }
+
+    #[test]
+    fn test_common_chars() {
+        assert_eq!(aoc_2::common_chars("abc", "acc"), "ac");
+    }
+
+    #[test]
+    fn test_closest_word() {}
 }
